3dd5517733f290fc6fae5dcbc85ad7e360a652c0
Shammas512 <shammasasif7@gmail.com>
2026-09-21 20:26:39 +0500

    Checking for perfect number

--- giki_Two.cpp
+++ giki_Two.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-
-int main(){
-  int n;
+int checkGreat(){
+     int n;
   cout << "Enter the number";
   cin >> n;
   int greatest = 0;
@@ -20,4 +19,45 @@ while (n>0)
 }
 cout << greatest;
 return 0;
+}
+
+int perfectNumber(){
+ int n ;
+ cout << "Enter the number" <<endl;
+ cin >> n;
+ vector <int> factors;
+ int sum = 0; 
+
+  for (int i = 1; i*i < n; i++)
+  {
+    if(n%i==0){
+        factors.push_back(i);
+        if (n/i==n) continue;
+        
+        
+        if (n/i != i){
+        factors.push_back(n/i);
+        
+       }
+        
+        
+        }
+        
+    };
+    sort(factors.begin(),factors.end());
+    for (auto it : factors) cout <<  " " << it <<endl;
+    for(auto it : factors) sum = sum + it;
+    cout <<  "Sum is " <<sum << endl;
+    if (sum==n)
+    {
+      cout << "Perfect Number";
+    }
+    else cout << "Not perfect";
+    
+    return 0 ;
+  }
+
+int main(){
+ perfectNumber();
+ return 0 ;
 }
